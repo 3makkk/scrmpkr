35b2a66667603ba09ba7eb723373f191fd5e8d2c
Sven Friedemann <sven.friedemann@bitgrip.de>
2025-11-07 20:06:08 +0100

    feat: integrate Sentry for error tracking and monitoring

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import React from "react";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://6f2afb7439faac05535aa70414c9462e@o4510325216575488.ingest.de.sentry.io/4510325224177744",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -8,13 +9,23 @@ export default defineConfig({
         plugins: ["babel-plugin-react-compiler"],
       },
     }),
+    sentryVitePlugin({
+      org: "erstellbar",
+      project: "javascript-react",
+    }),
   ],
+
   server: {
     port: 5173,
   },
+
   test: {
     environment: "jsdom",
     globals: true,
     include: ["__tests__/**/*.test.ts", "__tests__/**/*.test.js"],
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
